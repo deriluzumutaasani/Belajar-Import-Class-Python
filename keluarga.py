dc63a15a1160b1bae6ee6b9617ddfc1be2b8f6c0
Who am i <147464139+deriluzumutaasani@users.noreply.github.com>
2023-12-18 22:17:49 +0800

    Add files via upload

---
+++ keluarga.py
@@ -0,0 +1,10 @@
+from utama import keluarga
+
+klrg1 = keluarga("Syamsuddin", "Ayah", "Laki-laki", 72)
+print(f"Nama {klrg1.nama} peran {klrg1.peran} jenis kelamin {klrg1.jenis_kelamin} dengan usia {klrg1.usia}")
+
+klrg2 = keluarga("Shohnim", "Ibu", "Perempuan", 38)
+print(f"Nama {klrg2.nama} peran {klrg2.peran} jenis kelamin {klrg2.jenis_kelamin} dengan usia {klrg2.usia}")
+
+klrg3 = keluarga("Deri", "Anak", "Laki-laki", 20)
+print(f"Nama {klrg3.nama} peran {klrg3.peran} jenis kelamin {klrg3.jenis_kelamin} dengan usia {klrg3.usia}")
---
+++ pasien.py
@@ -0,0 +1,4 @@
+from utama import Pasien
+
+psn = Pasien("Hayyi", "Laki-laki" , "Demam", 4, 9)
+print(f"Nama {psn.nama} jenis kelamin {psn.jenis_kelamin} dengan keluhan {psn.keluhan} tanggal datang {psn.tanggal_datang} jam datang {psn.jam_kedatangan}")
